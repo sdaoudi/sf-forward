41bf9357332fb57bb20c704d9b3d047e10966665
DAOUDI Soufian <soufian.daoudi2@gmail.com>
2018-03-18 00:36:21 +0100

    Fixed scrutinizer patches 2

--- Util/RequestMethodMapping.php
+++ Util/RequestMethodMapping.php
@@ -5,29 +5,28 @@
  * Date: 13/03/2018
  * Time: 10:37
  */
-
 namespace SfForward\Util;
 
 class RequestMethodMapping
 {
-	const REQUEST_METHOD_GET = 'get';
-	const REQUEST_METHOD_POST = 'post';
+    const REQUEST_METHOD_GET    = 'get';
+    const REQUEST_METHOD_POST   = 'post';
 
-	const REQUEST_QUERY_GET = 'query';
-	const REQUEST_QUERY_POST = 'request';
+    const REQUEST_QUERY_GET     = 'query';
+    const REQUEST_QUERY_POST    = 'request';
 
-	static public $methodsMapping = [
-		self::REQUEST_METHOD_GET => self::REQUEST_QUERY_GET,
-		self::REQUEST_METHOD_POST => self::REQUEST_QUERY_POST,
-	];
+    static public $methodsMapping = [
+        self::REQUEST_METHOD_GET    => self::REQUEST_QUERY_GET,
+        self::REQUEST_METHOD_POST   => self::REQUEST_QUERY_POST,
+    ];
 
-	/**
-	 * @param string $method
-	 *
-	 * @return bool
-	 */
-	public static function isValidMethod($method)
-	{
-		return isset(self::$methodsMapping[ $method ]);
-	}
+    /**
+     * @param string $method
+     *
+     * @return bool
+     */
+    public static function isValidMethod($method)
+    {
+        return isset(self::$methodsMapping[ $method ]);
+    }
 }
