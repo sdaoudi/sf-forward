d61480b1cfce11ea7df7faab86134cb8e9fc8d6e
DAOUDI Soufian <soufian.daoudi2@gmail.com>
2018-03-24 23:53:27 +0100

    Fixed post form params

--- Util/RequestMethodMapping.php
+++ Util/RequestMethodMapping.php
@@ -15,9 +15,17 @@ class RequestMethodMapping
     const REQUEST_QUERY_GET     = 'query';
     const REQUEST_QUERY_POST    = 'request';
 
+    const CONTENT_TYPE_GET     = 'query';
+    const CONTENT_TYPE_POST    = 'form_params';
+
     static public $methodsMapping = [
         self::REQUEST_METHOD_GET    => self::REQUEST_QUERY_GET,
         self::REQUEST_METHOD_POST   => self::REQUEST_QUERY_POST,
+    ];
+
+    static public $contentTypes = [
+        self::REQUEST_METHOD_GET    => self::CONTENT_TYPE_GET,
+        self::REQUEST_METHOD_POST   => self::CONTENT_TYPE_POST,
     ];
 
     /**
