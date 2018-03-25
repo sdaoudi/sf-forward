8981282747e60e92d427a583a63cea1f3ae55d8c
DAOUDI Soufian <soufian.daoudi2@gmail.com>
2018-03-25 05:13:51 +0200

    Update Response var annotation

--- Manager/ResponseManager.php
+++ Manager/ResponseManager.php
@@ -17,15 +17,15 @@ use Symfony\Component\HttpFoundation\Response;
 
 class ResponseManager
 {
-	/**
-	 * @var GuzzleResponse
-	 */
-	protected $response;
+    /**
+     * @var ResponseInterface
+     */
+    protected $response;
 
-	/**
-	 * @var string
-	 */
-	protected $projectDir;
+    /**
+     * @var string
+     */
+    protected $projectDir;
 
     /**
      * ResponseManager constructor.
@@ -33,55 +33,55 @@ class ResponseManager
      * @param ResponseInterface $response
      * @param string $projectDir
      */
-	public function __construct(ResponseInterface $response, $projectDir = __DIR__)
-	{
-		$this->response   = $response;
-		$this->projectDir = $projectDir;
-	}
+    public function __construct(ResponseInterface $response, $projectDir = __DIR__)
+    {
+        $this->response   = $response;
+        $this->projectDir = $projectDir;
+    }
 
-	/**
-	 * @return BinaryFileResponse|Response
-	 */
-	public function getResponse()
-	{
-		if ($this->response->hasHeader('Content-Disposition')) {
-			return $this->getBinaryFileResponse();
-		}
+    /**
+     * @return BinaryFileResponse|Response
+     */
+    public function getResponse()
+    {
+        if ($this->response->hasHeader('Content-Disposition')) {
+            return $this->getBinaryFileResponse();
+        }
 
-		return $this->getHttpResponse();
-	}
+        return $this->getHttpResponse();
+    }
 
-	/**
-	 * @return BinaryFileResponse
-	 */
-	protected function getBinaryFileResponse()
-	{
-		$projectDir = PublicDirectory::getPublicDir($this->projectDir);
-		$filename = $projectDir.mt_rand();
+    /**
+     * @return BinaryFileResponse
+     */
+    protected function getBinaryFileResponse()
+    {
+        $projectDir = PublicDirectory::getPublicDir($this->projectDir);
+        $filename = $projectDir.mt_rand();
 
-		$fileSystem = new Filesystem();
-		$fileSystem->dumpFile(
-			$filename,
-			$this->response->getBody()->getContents()
-		);
+        $fileSystem = new Filesystem();
+        $fileSystem->dumpFile(
+            $filename,
+            $this->response->getBody()->getContents()
+        );
 
-		return (
-			new BinaryFileResponse(
-				$filename,
-				$this->response->getStatusCode(),
-				$this->response->getHeaders()
-			)
-		)->deleteFileAfterSend(true);
-	}
+        return (
+            new BinaryFileResponse(
+                $filename,
+                $this->response->getStatusCode(),
+                $this->response->getHeaders()
+            )
+        )->deleteFileAfterSend(true);
+    }
 
-	/**
-	 * @return Response
-	 */
-	protected function getHttpResponse()
-	{
-		$response = new Response($this->response->getBody());
-		$response->setStatusCode($this->response->getStatusCode());
+    /**
+     * @return Response
+     */
+    protected function getHttpResponse()
+    {
+        $response = new Response($this->response->getBody());
+        $response->setStatusCode($this->response->getStatusCode());
 
-		return $response;
-	}
+        return $response;
+    }
 }
